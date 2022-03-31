1b837c207cf5d6d69ddc0b6c3eaaf578b532cfe7
MrOlegBar <Legan26021991@yandex.ru>
2022-03-31 22:32:40 +0300

    На пути к успеху!

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,60 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int[] january = new int[31];
+        int[] february = new int[28];
+        int[] march = new int[31];
+        int[] april = new int[30];
+        int[] may = new int[31];
+        int[] june = new int[30];
+        int[] july = new int[31];
+        int[] august = new int[31];
+        int[] september = new int[30];
+        int[] october = new int[31];
+        int[] november = new int[30];
+        int[] december = new int[31];
+        StepTracker stepTracker = new StepTracker();
         while (true) {
             printMenu();
             int command = scanner.nextInt();
 
             if (command == 1) {
-                System.out.println("Предлагаю выбрать дату, за которую хотите ввести количество шагов");
-                System.out.println("Выберите месяц: 01-Янв | 02-Фев | 03-Мар | 04-Апр | 05-Май | 06-Июн \n\t\t\t\t07-Июл | 08-Авг | 09-Сен | 10-Окт | 11-Ноя | 12-Дек");
+                System.out.println("Выберите месяц:  1-Янв |  2-Фев |  3-Мар |  4-Апр |  5-Май |  6-Июн \n\t\t\t\t" +
+                        " 7-Июл |  8-Авг |  9-Сен | 10-Окт | 11-Ноя | 12-Дек");
                 int month = scanner.nextInt();
                 if (month == 1) {
-                    System.out.println("Выберите число:");
-                    for (int i = 1; i < 32; i += 7) {
-                        int n = i;
-
-                        for (int j = n; j < (n + 7) & j < 32; j++) {
-                            System.out.print("\t" + j + "\t|");
-                        }
-
-                        System.out.println("");
-                    }
-
-
-
-
+                    stepTracker.selectDay(scanner, january);
+                } else if (month == 2) {
+                    stepTracker.selectDay(scanner, february);
+                } else if (month == 3) {
+                    stepTracker.selectDay(scanner, march);
+                } else if (month == 4) {
+                    stepTracker.selectDay(scanner, april);
+                } else if (month == 5) {
+                    stepTracker.selectDay(scanner, may);
+                } else if (month == 6) {
+                    stepTracker.selectDay(scanner, june);
+                } else if (month == 7) {
+                    stepTracker.selectDay(scanner, july);
+                } else if (month == 8) {
+                    stepTracker.selectDay(scanner, august);
+                } else if (month == 9) {
+                    stepTracker.selectDay(scanner, september);
+                } else if (month == 10) {
+                    stepTracker.selectDay(scanner, october);
+                } else if (month == 11) {
+                    stepTracker.selectDay(scanner, november);
+                } else if (month == 12) {
+                    stepTracker.selectDay(scanner, december);
+                } else {
+                    System.out.println("В году 12 месяцев =)");
                 }
 
-                //converter.convert(moneyBeforeSalary, currency);
+
             } else if (command == 2) {
-                //dinnerAdvisor.getAdvice(moneyBeforeSalary, daysBeforeSalary);
+                //
             } else if (command == 3) {
-                /*System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
-                int day = scanner.nextInt();
-                System.out.println("Введите размер траты:");
-                double expense = scanner.nextDouble();
-
-                moneyBeforeSalary = expensesManager.saveExpense(moneyBeforeSalary, expense, day);*/
-
-
+                //
             } else if (command == 0) {
                 System.out.println("Выход");
                 break;
@@ -49,6 +65,7 @@ public class Main {
             }
         }
     }
+
 
     private static void printMenu() {
         System.out.println("Что вы хотите сделать?");
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,3 +1,38 @@
-public class StepTracker {
+import java.util.Scanner;
 
+public class StepTracker {
+    public static void selectDay(Scanner scanner, int[] month) {
+        int[] monthExample = new int[month.length];
+
+        System.out.println("Выберите число:");
+        for (int i = 1; i <= monthExample.length; i += 7) {
+            for (int j = i; j < (i + 7) & (j <= monthExample.length); j++) {
+                monthExample[j-1] = j;
+                System.out.print("\t" + monthExample[j-1] + "\t|");
+            }
+            System.out.println("");
+        }
+
+        int day = scanner.nextInt();
+
+        if (day < 1 | day > month.length) {
+            System.out.println("В этом месяце нет такого дня =)");
+            return;
+        } else {
+            System.out.println("Введите количество шагов:");
+            int numberSteps = scanner.nextInt();
+            if (numberSteps < 0) {
+                System.out.println("Шаги задом на перёд не засчитываются =)");
+                return;
+            } else {
+                month[day-1] = numberSteps;
+                System.out.println("Введенное количество шагов: " + month[day-1] + " сохранено.");
+                for (int i = 0; i < month.length; i++) {
+                    System.out.print((i + 1) + " день: " + month[i] + " шагов ");
+                }
+
+
+            }
+        }
+    }
 }
