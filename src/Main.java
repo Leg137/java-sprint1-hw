11181533c8e4f9146a13be4c9f430bbb96737d7a
MrOlegBar <Legan26021991@yandex.ru>
2022-04-01 16:56:20 +0300

    Делал декомпозицию

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] january = new int[31];
-        int[] february = new int[28];
-        int[] march = new int[31];
-        int[] april = new int[30];
-        int[] may = new int[31];
-        int[] june = new int[30];
-        int[] july = new int[31];
-        int[] august = new int[31];
-        int[] september = new int[30];
-        int[] october = new int[31];
-        int[] november = new int[30];
-        int[] december = new int[31];
+
         StepTracker stepTracker = new StepTracker();
+
         while (true) {
             printMenu();
             int command = scanner.nextInt();
@@ -24,33 +14,10 @@ public class Main {
                 System.out.println("Выберите месяц:  1-Янв |  2-Фев |  3-Мар |  4-Апр |  5-Май |  6-Июн \n\t\t\t\t" +
                         " 7-Июл |  8-Авг |  9-Сен | 10-Окт | 11-Ноя | 12-Дек");
                 int month = scanner.nextInt();
-                if (month == 1) {
-                    stepTracker.selectDay(scanner, january);
-                } else if (month == 2) {
-                    stepTracker.selectDay(scanner, february);
-                } else if (month == 3) {
-                    stepTracker.selectDay(scanner, march);
-                } else if (month == 4) {
-                    stepTracker.selectDay(scanner, april);
-                } else if (month == 5) {
-                    stepTracker.selectDay(scanner, may);
-                } else if (month == 6) {
-                    stepTracker.selectDay(scanner, june);
-                } else if (month == 7) {
-                    stepTracker.selectDay(scanner, july);
-                } else if (month == 8) {
-                    stepTracker.selectDay(scanner, august);
-                } else if (month == 9) {
-                    stepTracker.selectDay(scanner, september);
-                } else if (month == 10) {
-                    stepTracker.selectDay(scanner, october);
-                } else if (month == 11) {
-                    stepTracker.selectDay(scanner, november);
-                } else if (month == 12) {
-                    stepTracker.selectDay(scanner, december);
-                } else {
-                    System.out.println("В году 12 месяцев =)");
-                }
+
+                System.out.println("Выберите число:");
+                stepTracker.savingSteps(month);
+
 
 
             } else if (command == 2) {
@@ -65,7 +32,6 @@ public class Main {
             }
         }
     }
-
 
     private static void printMenu() {
         System.out.println("Что вы хотите сделать?");
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,20 +1,65 @@
 import java.util.Scanner;
 
 public class StepTracker {
-    public static void selectDay(Scanner scanner, int[] month) {
-        int[] monthExample = new int[month.length];
 
-        System.out.println("Выберите число:");
-        for (int i = 1; i <= monthExample.length; i += 7) {
-            for (int j = i; j < (i + 7) & (j <= monthExample.length); j++) {
-                monthExample[j-1] = j;
-                System.out.print("\t" + monthExample[j-1] + "\t|");
-            }
-            System.out.println("");
+    Scanner scanner = new Scanner(System.in);
+    int[] january = new int[31];
+    int[] february = new int[28];
+    int[] march = new int[31];
+    int[] april = new int[30];
+    int[] may = new int[31];
+    int[] june = new int[30];
+    int[] july = new int[31];
+    int[] august = new int[31];
+    int[] september = new int[30];
+    int[] october = new int[31];
+    int[] november = new int[30];
+    int[] december = new int[31];
+    int goalSteps = 10000;
+
+    void savingSteps(int numberMonth) {
+        if (numberMonth == 1) {
+            monthExample(january);
+            savingStepsPerDay(scanner, january);
+        } else if (numberMonth == 2) {
+            monthExample(february);
+            savingStepsPerDay(scanner, february);
+        } else if (numberMonth == 3) {
+            monthExample(march);
+            savingStepsPerDay(scanner, march);
+        } else if (numberMonth == 4) {
+            monthExample(april);
+            savingStepsPerDay(scanner, april);
+        } else if (numberMonth == 5) {
+            monthExample(may);
+            savingStepsPerDay(scanner, may);
+        } else if (numberMonth == 6) {
+            monthExample(june);
+            savingStepsPerDay(scanner, june);
+        } else if (numberMonth == 7) {
+            monthExample(july);
+            savingStepsPerDay(scanner, july);
+        } else if (numberMonth == 8) {
+            monthExample(august);
+            savingStepsPerDay(scanner, august);
+        } else if (numberMonth == 9) {
+            monthExample(september);
+            savingStepsPerDay(scanner, september);
+        } else if (numberMonth == 10) {
+            monthExample(october);
+            savingStepsPerDay(scanner, october);
+        } else if (numberMonth == 11) {
+            monthExample(november);
+            savingStepsPerDay(scanner, november);
+        } else if (numberMonth == 12) {
+            monthExample(december);
+            savingStepsPerDay(scanner, december);
+        } else {
+            System.out.println("В году 12 месяцев =)");
         }
-
+    }
+    void savingStepsPerDay(Scanner scanner, int[] month) {
         int day = scanner.nextInt();
-
         if (day < 1 | day > month.length) {
             System.out.println("В этом месяце нет такого дня =)");
             return;
@@ -25,8 +70,8 @@ public class StepTracker {
                 System.out.println("Шаги задом на перёд не засчитываются =)");
                 return;
             } else {
-                month[day-1] = numberSteps;
-                System.out.println("Введенное количество шагов: " + month[day-1] + " сохранено.");
+                month[day - 1] = numberSteps;
+                System.out.println("Введенное количество шагов: " + month[day - 1] + " сохранено.");
                 for (int i = 0; i < month.length; i++) {
                     System.out.print((i + 1) + " день: " + month[i] + " шагов ");
                 }
@@ -34,5 +79,18 @@ public class StepTracker {
 
             }
         }
+    }
+    void monthExample(int[] month) {
+        int[] monthExample = new int[month.length];
+        for (int i = 1; i <= month.length; i += 7) {
+            for (int j = i; j <= (i + 7) & (j <= month.length); j++) {
+                monthExample[j - 1] = j;
+                System.out.print("\t" + monthExample[j - 1] + "\t|");
+            }
+            System.out.println("");
+        }
+    }
+    void statistics() {
+
     }
 }
